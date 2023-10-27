f44351ee7a9ab0cc72720bec7a741e02fd62568e
Casimir Lundberg <clundber@c2r4p8.hive.fi>
2023-10-27 14:16:39 +0300

    fixed

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,15 +6,13 @@
 /*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 12:16:15 by clundber          #+#    #+#             */
-/*   Updated: 2023/10/27 12:58:46 by clundber         ###   ########.fr       */
+/*   Updated: 2023/10/27 14:14:54 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 
 {
-	char	*sptr;
-
 	while (*s != '\0')
 	{
 		if (c == *s)
