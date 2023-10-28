723c6afe2680558a3c3875bf13e8dfbf6af93c10
welhox <casimir.lundberg@gmail.com>
2023-10-28 22:38:58 +0300

    kotona

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: welhox <welhox@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 18:18:03 by clundber          #+#    #+#             */
-/*   Updated: 2023/10/27 18:40:40 by clundber         ###   ########.fr       */
+/*   Updated: 2023/10/28 22:38:14 by welhox           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,22 @@ int	ft_atoi(const char *str)
 {
 	int	num;
 	int	i;
+	int	neg;
 
+	neg = 0;
 	i = 0;
 	num = 0;
-	if (str[0] == '-')
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	neg = i;
+	if (str[i] == '-' || str[i] == '+')
 		i++;
 	while (str[i] >= 48 && str[i] <= 57)
 	{
 		num = (num * 10) + (str[i] - 48);
 		i++;
 	}
-	if (str[0] == '-')
+	if (str[neg] == '-')
 		num *= -1;
 	return (num);
 }
