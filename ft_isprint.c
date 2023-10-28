91818780dd339489625ce39f747ad12398a1094e
welhox <casimir.lundberg@gmail.com>
2023-10-29 00:01:32 +0300

    kotona korjattu

--- ft_isprint.c
+++ ft_isprint.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: welhox <welhox@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:35:46 by clundber          #+#    #+#             */
-/*   Updated: 2023/10/25 16:41:53 by clundber         ###   ########.fr       */
+/*   Updated: 2023/10/28 23:59:34 by welhox           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isprint(int c)
 {
-	if (c >= 32 && c <= 127)
+	if (c >= 32 && c <= 126)
 		return (1);
 	else
 		return (0);
