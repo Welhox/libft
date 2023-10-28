04287ce917a83cb9eaf959f59840099a84b87502
welhox <casimir.lundberg@gmail.com>
2023-10-28 22:42:31 +0300

    kotona korjattu

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: welhox <welhox@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:57:59 by clundber          #+#    #+#             */
-/*   Updated: 2023/10/26 16:08:43 by clundber         ###   ########.fr       */
+/*   Updated: 2023/10/28 22:41:57 by welhox           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	dstptr = dst;
 	srcptr = src;
-	while (n > 0 && srcptr != '\0')
+	while (n > 0)
 	{
 		*dstptr = *srcptr;
 		dstptr++;
