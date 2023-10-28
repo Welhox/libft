0199af52a17c74b6cb87da779950e02763ed21b2
welhox <casimir.lundberg@gmail.com>
2023-10-28 23:02:39 +0300

    kotona korjattu

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: welhox <welhox@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:30:33 by clundber          #+#    #+#             */
-/*   Updated: 2023/10/27 17:35:05 by clundber         ###   ########.fr       */
+/*   Updated: 2023/10/28 23:02:18 by welhox           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	s1ptr = (unsigned char *)s1;
 	s2ptr = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
 	while (*s1ptr == *s2ptr && n > 0)
 	{
 		s1ptr++;
 		s2ptr++;
 		n--;
 	}
+	if (n == 0 && *s1ptr-- == *s2ptr--)
+		return (0);
 	if (*s1ptr != *s2ptr)
 		return (*s1ptr - *s2ptr);
 	return (0);
